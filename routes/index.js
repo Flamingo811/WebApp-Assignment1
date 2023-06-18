1b2e0efd07fb9f72f92a7c1a59957c49abbc0bbe
Flamingo811 <129621208+Flamingo811@users.noreply.github.com>
2023-06-18 02:27:17 -0400

    1st

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,6 @@ const bcrypt = require('bcryptjs');
 const session = require('express-session');
 const User = require('../models/User');
 
-mongoose.connect('mongodb+srv://stephencencol:Kw811822@cluster0.x5e3n7s.mongodb.net/?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
 
 
 router.use(express.urlencoded({ extended: true }));
