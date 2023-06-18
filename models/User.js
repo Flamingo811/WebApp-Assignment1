b8c7740339baa1c105c5f05c468b7c967f458b3a
Flamingo811 <129621208+Flamingo811@users.noreply.github.com>
2023-06-18 15:30:02 -0400

    1st

--- models/User.js
+++ models/User.js
@@ -4,15 +4,13 @@
         Student ID: 301255100
         Date: 18-June-2023
 */
-    const mongoose = require('mongoose');
-    const Schema = mongoose.Schema;
-    
-    const userSchema = new Schema({
-        username: String,
-        password: String,
-        email: String,
-    });
-    
-    const User = mongoose.model('User', userSchema);
-    
-    module.exports = User;
+ const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true },
+  // Add other relevant fields here
+});
+
+module.exports = mongoose.model('User', userSchema);
